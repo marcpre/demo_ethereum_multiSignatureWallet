392f7a468b1c097fd005009d411a4fc7c543f208
marcpre <marcus.presich@gmail.com>
2018-02-10 12:03:50 +0100

    fixing multi signature wallet

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,102 +1,115 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
+import '../stylesheets/app.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
+import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import myWallet_artifact from '../../build/contracts/MyWallet.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+// Wallet is our usable abstraction, which we'll use through the code below.
+var MyWallet = contract(myWallet_artifact)
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
+var accounts
+var account
 
 window.App = {
-  start: function() {
-    var self = this;
+  start: function () {
+    var self = this
 
     // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    MyWallet.setProvider(web3.currentProvider)
 
     // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
+    web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
-        alert("There was an error fetching your accounts.");
+        alert('There was an error fetching your accounts.')
         return;
       }
 
       if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
         return;
       }
 
-      accounts = accs;
-      account = accounts[0];
+      accounts = accs
+      account = accounts[0]
 
-      self.refreshBalance();
-    });
+      document.getElementById('addresses').innerHTML = accounts.join('<br />')
+
+      App.basicInfoUpdate()
+      App.listenToEvents()
+    })
   },
 
-  setStatus: function(message) {
-    var status = document.getElementById("status");
-    status.innerHTML = message;
+  basicInfoUpdate: function () {
+    MyWallet.deployed().then(function (instance) {
+      document.getElementById('walletAddress').innerHTML = instance.address
+      document.getElementById('walletEther').innerHTML = web3.fromWei(web3.eth.getBalance(instance.address).toNumber(), 'ether')
+    })
   },
 
-  refreshBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
+  listenToEvents: function () {
+    MyWallet.deployed().then(function (instance) {
+      instance.receivedFunds({}, { fromBlock:0, toBlock:'latest' }).watch(function (error, event) {
+        document.getElementById('fundEvents').innerHTML += JSON.stringify(event)
+      })
+      instance.proposalReceived({}, { fromBlock:0, toBlock:'latest' }).watch(function (error, event) {
+        document.getElementById('proposalEvents').innerHTML += JSON.stringify(event)
+      })
+    })
   },
 
-  sendCoin: function() {
-    var self = this;
+  confirmTransaction: function () {
+    // YOUR PART
+  },
 
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
+  submitEtherToWallet: function () {
+    MyWallet.deployed().then(function (instance) {
+      return instance.sendTransaction({ from: account, to: instance.address, value: web3.toWei(5, 'ether') })
 
-    this.setStatus("Initiating transaction... (please wait)");
+    }).then(function (result) {
+      App.basicInfoUpdate()
+    })
+  },
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
+  submitTransaction: function () {
+    var _to = document.getElementById('to').value
+    var _amount = parseInt(document.getElementById('amount').value)
+    var _reason = document.getElementById('reason').value
+
+    MyWallet.deployed().then(function (instance) {
+      return instance.spendMoneyOn(_to, web3.toWei(_amount, 'finney'), _reason, { from:accounts[2], gas:500000 })
+    }).then(function (result) {
+      console.log(result)
+      App.basicInfoUpdate()
+    }).catch(function (err) {
+      console.error(err)
+    })
+  },
+
+  sendCoin: function () {
+    var self = this
   }
-};
+}
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    window.web3 = new Web3(web3.currentProvider)
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask")
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
   }
 
-  App.start();
-});
+  App.start()
+})
--- contracts/MyWallet.sol
+++ contracts/MyWallet.sol
@@ -1,29 +1,6 @@
 pragma solidity ^0.4.15;
 
-contract owned {
-    address owner;
-
-    modifier onlyowner() {
-        /**
-         * Update Exception Handling from Solidity 0.4.13!
-         * See: https://vomtom.at/exception-handling-in-solidity/
-         * If you have any questions, head over to the course Q&A!
-         **/
-        require(msg.sender == owner);
-        _;
-    }
-
-    function owned() public {
-        owner = msg.sender;
-    }
-}
-
-contract mortal is owned {
-    function kill() public {
-        if (msg.sender == owner)
-        selfdestruct(owner);
-    }
-}
+import "./mortal.sol";
 
 contract MyWallet is mortal {
     event receivedFunds(address _from, uint256 _amount);
